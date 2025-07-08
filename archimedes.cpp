a8304d3d92827883f8331f063de208c6a1fbd21a
justinmeredith <jm.4444@icloud.com>
2025-07-08 17:33:12 -0500

    Adds basic mathematical functions and a function that can turn the user's input into a series of tokens stored in a vector, using white spaces as a delimiter. Still requires very specific formatting to be used by the user and does not do any mathematical operations yet.

--- archimedes.cpp
+++ archimedes.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cctype>
+#include <vector>
 using namespace std;
 
 // -__VARIABLES__-
@@ -36,10 +37,63 @@ string getUserCalculation() {
     return input_string;
 }
 
+// Turns the input string into a vector
+vector<string> tokenizeStringAsVector(string input_string) {
+    vector<string> tokenized_string;
+    string creating_element;
+    for (int i = 0; i < input_string.length(); i++) {
+        if (input_string[i] == ' ') {
+            tokenized_string.push_back(creating_element);
+            creating_element = "";
+        } else if (i == (input_string.length() - 1)) {
+            creating_element += input_string[i];
+            tokenized_string.push_back(creating_element);
+        } else {
+            creating_element += input_string[i];
+        }
+    }
+
+    return tokenized_string;
+}
+
+// Performs operations on the user's input in the order of PEMDAS
+int performOperations(string input_string) {
+    // placeholder
+}
+
+// Adds two numbers
+int archAdd(int num1, int num2) {
+    return num1 + num2;
+}
+
+// Subtracts num2 from num1
+int archSubtract(int num1, int num2) {
+    return num1 - num2;
+}
+
+// Multiplies two numbers
+int archMultiply(int num1, int num2) {
+    return num1 * num2;
+}
+
+// Divides num1 by num2
+int archDivide(int num1, int num2) {
+    return num1 / num2;
+}
+
+// -__MAIN LOOP__-
 int main() {
 
     user_input = getUserCalculation();
-    cout << "Your requested calculation is " << user_input;
+    cout << "Your requested calculation is " << user_input << endl;
+
+    vector<string> conversion_test = tokenizeStringAsVector(user_input);
+
+    cout << "The tokens in your requested calculation are: " << endl;
+
+    for (int i = 0; i < conversion_test.size(); i++) {
+        cout << "->" << conversion_test[i] << endl;
+    }
 
     return 0;
 }
