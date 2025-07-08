4231b155b1c49ee4343f3d4bf6aa18d0cb8a5313
justinmeredith <jm.4444@icloud.com>
2025-07-08 11:51:32 -0500

    Program reads and writes a line of input.

--- archimedes.cpp
+++ archimedes.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+string user_input;
+
 int main() {
-    cout << "Heyo";
+    cout << "Please enter your calculation.\n     > ";
+    getline(cin, user_input);
+    cout << "Your requested calculation is " << user_input;
     return 0;
 }
