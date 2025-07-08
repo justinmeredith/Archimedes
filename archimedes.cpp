e0715b5687a5594f0c9ecd4a98e34bfd02f13897
justinmeredith <jm.4444@icloud.com>
2025-07-08 12:28:38 -0500

    Adds input validation to make sure alphabetic characters aren't being entered and includes some organizing of functions and variables.

--- archimedes.cpp
+++ archimedes.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
+#include <cctype>
 using namespace std;
 
+// -__VARIABLES__-
 string user_input;
 
+// -__FUNCTIONS__-
+
+// Checks that only accepted characters are entered by the user
+bool validateUserInput(string input_string) {
+    bool validity = true;
+    for (int i = 0; i < input_string.length(); i++) {
+        if (isalpha(input_string[i])) {
+            validity = false;
+        }
+    }
+
+    return validity;
+}
+
+// Gets input from the user to use in the calculation
 string getUserCalculation() {
-    string input;
-    cout << "Please enter your calculation.\n     > ";
-    getline(cin, input);
-    return input;
+    string input_string;
+    bool valid_input = false;
+    while (valid_input == false) {
+        cout << "\nPlease enter your calculation.\n     > ";
+        getline(cin, input_string);
+        if (validateUserInput(input_string)) {
+            valid_input = true;
+        } else {
+            cout << endl << "     **Error** Archimedes cannot process alphabetic characters or variables at this time." << endl;
+        }
+    }
+
+    return input_string;
 }
 
 int main() {
